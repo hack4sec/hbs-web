dbcaa1a698aa0eadd0920e50717fd7125e9d7308
a.kuzmin <anton.kuzmin@marketgid.com>
2016-07-23 12:35:29 +0300

    cracked and uncracked coutners of hashlists

--- application/models/Hashlist.php
+++ application/models/Hashlist.php
@@ -3,17 +3,16 @@
 class Hashlist extends Zend_Db_Table_Row
 {
     protected $_tableClass = 'Hashlists';
-
     private $_counts;
+
     public function getCounts() {
         if (!$this->_counts) {
-            $Hashes = new Hashes();
-            $this->_counts['cracked'] = $Hashes->getCountByHashlistId($this->id, 1);
-            $this->_counts['not_cracked'] = $Hashes->getCountByHashlistId($this->id, 0);
-            $this->_counts['cracked_percents'] = $this->_counts['not_cracked'] ?
-                                                 round($this->_counts['cracked'] / ($this->_counts['not_cracked']+$this->_counts['cracked']) * 100, 1) :
+            $this->_counts['cracked'] = $this->cracked;
+            $this->_counts['not_cracked'] = $this->uncracked;
+            $this->_counts['cracked_percents'] = $this->uncracked ?
+                                                 round($this->cracked / ($this->uncracked + $this->cracked) * 100, 1) :
                                                  0;
-            $this->_counts['not_cracked_percents'] =  $this->_counts['not_cracked'] ? 100 - $this->_counts['cracked_percents'] : 0;
+            $this->_counts['not_cracked_percents'] =  $this->uncracked ? 100 - $this->_counts['cracked_percents'] : 0;
         }
         return $this->_counts;
     }
